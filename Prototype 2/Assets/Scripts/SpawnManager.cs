a11f2986e9799f3080aaf9140c769fa0fec834d8
emsmariee <751990@student.dusd.net>
2023-03-08 11:39:43 -0800

    Created more scripts
    Fixed my scripts so multiple animals can spawn randomly, fixed box collider

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,22 +7,27 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPreFabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPreFabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-
-            Instantiate(animalPreFabs[animalIndex], new Vector3(0, 0, 20), animalPreFabs[animalIndex].transform.rotation);
-        }
+       
     }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPreFabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPreFabs[animalIndex], spawnPos, animalPreFabs[animalIndex].transform.rotation);
+    }
+
 }
